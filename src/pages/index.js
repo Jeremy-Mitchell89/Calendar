80c49860317f259073e7b6b65575b58ef3cc9a5c
Jeremy <Johnbovine@yahoo.com>
2019-12-30 19:49:11 -0600

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import timeGridPlugin from "@fullcalendar/timegrid"
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import "@fullcalendar/core/main.css"
+import "@fullcalendar/daygrid/main.css"
+import "@fullcalendar/timegrid/main.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <FullCalendar
+      className="demo-app-calendar"
+      defaultView="dayGridMonth"
+      header={{
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+      }}
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+    />
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
